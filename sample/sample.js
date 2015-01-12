bcfe836bbaed6c6fc7cb0d044c43dcfe5d769510
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-11 21:49:22 -0700

    updated runtime and star example

--- sample/sample.js
+++ sample/sample.js
@@ -13,37 +13,36 @@ MiniCanvas.create(200, 200)
   .dotGrid(12, 12, 1, "rgba(255,255,255,0.75)")
   .display("box");
 
-MiniCanvas.create(200, 200)
-  .with(function(mini, w, h) {
-    var stars = 200,
-        frames = 40,
-        arr = [],
-        i, j, star;
-    for(i = 0; i < stars; i++) {
-      arr.push({
-        x : Math.random() * w,
-        y : Math.random() * h,
-        r : Math.random() + 1.0,
-        s : Math.random() + 0.5
-      });
-    }
+var w = 200,
+    h = 200,
+    interaction = MiniCanvas.create(w, h).animate(),
+    frames = 40,
+    stars = 200,
+    arr = [],
+    i, j, star;
 
-    for(i = 0; i < frames; i++) {
+for(i = 0; i < stars; i++) {
+  arr.push({
+    x : Math.random() * w,
+    y : Math.random() * h,
+    r : Math.random() + 1.0,
+    s : Math.random() + 0.5
+  });
+}
+
+for(i = 0; i < frames; i++) {
+  (function(i) {
+    interaction.frame(function(mini) {
       mini.fill("#000000");
       var t = (i / frames) < 0.5 ? (i / frames) + 0.5 : 1.5 - (i / frames);
       for(j = 0; j < stars; j++) {
         star = arr[j];
-        mini
-          .dot(star.x, star.y, star.r * t, "rgba(255,255,255,"+t+")");
+        mini.dot(star.x, star.y, star.r * t, "rgba(255,255,255,"+t+")");
       }
-      mini.storeFrame();
-    }
-  })
+      //mini.storeFrame();
+    });
+  })(i);
+}
+
+interaction.done()
   .display("stars");
-/*
-MiniCanvas.create(500, 500)
-  .box(function(x, y) {
-    return 0xFFCCFFCC;
-  })
-  .display("box");
-*/
